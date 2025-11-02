afe0c29883304eb99756342c4cf5e059506e7d33
Abdelbari Saoutelhak <abdelbari.saoutelhak@etu.umontpellier.fr>
2025-11-02 21:23:16 +0100

    Update article "" via web interface

---
+++ data/articles/ddsbdbdfb-1762108095598.ts
@@ -0,0 +1,9 @@
+import { Article } from '../../types';
+
+export const ddsbdbdfb_1762108095598: Article = {
+  id: 'ddsbdbdfb-1762108095598',
+  title: '',
+  date: '2025-11-02',
+  summary: '',
+  content: ``,
+};
