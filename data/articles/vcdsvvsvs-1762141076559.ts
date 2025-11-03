de03799c96d53ab5183149b006a8e00672df8813
Abdelbari Saoutelhak <abdelbari.saoutelhak@etu.umontpellier.fr>
2025-11-03 06:08:36 +0100

    Update article "vcdsvvsvs" via web interface

--- data/articles/vcdsvvsvs-1762141076559.ts
+++ data/articles/vcdsvvsvs-1762141076559.ts
@@ -22,6 +22,5 @@ On a indiqué dans l'introduction de ce chapitre que l'intérêt de ce type de v
 Ainsi la dérivation et l'intégration d'une vibration monochromatique ne modifie que sa phase, ce qui explique leur rôle fondamental dans la modélisation des phénomènes oscillatoires et ondulatoires.
 \\begin{remarque}
 Ainsi, le signe négatif dans \\((\\omega t - \\phi)\\) traduit simplement le fait qu’une phase plus grande implique que la vibration atteint son maximum plus tard dans le temps.
-\\end{remarque}
-`,
+\\end{remarque}`,
 };
