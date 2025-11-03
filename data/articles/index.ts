bf46fa18681581fa30fada8c089135e58f0ba0d2
Abdelbari Saoutelhak <abdelbari.saoutelhak@etu.umontpellier.fr>
2025-11-03 04:56:45 +0100

    Update index.ts

--- data/articles/index.ts
+++ data/articles/index.ts
@@ -2,10 +2,8 @@ import { Article } from '../../types';
 import { theEulerIdentity } from './the-euler-identity';
 import { maxwellsEquationsInVacuum } from './maxwells-equations-in-vacuum';
 import { understandingSchrodingerEquation } from './understanding-schrodinger-equation';
-import { vibrations_1762116721386 } from './vibrations-1762116721386';
 
 export const articles: Article[] = [
-  vibrations_1762116721386,
   understandingSchrodingerEquation,
   maxwellsEquationsInVacuum,
   theEulerIdentity,
