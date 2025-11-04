fdc25060cb60953a531d178d84ab186e20924355
Abdelbari Saoutelhak <abdelbari.saoutelhak@etu.umontpellier.fr>
2025-11-04 15:08:36 +0100

    Add new article: ddfbdfbdfbdb

---
+++ data/articles/ddfbdfbdfbdb.ts
@@ -0,0 +1,9 @@
+import { Article } from '../../types';
+
+export const ddfbdfbdfbdb: Article = {
+  id: 'ddfbdfbdfbdb',
+  title: 'ddfbdfbdfbdb',
+  date: '2025-11-04',
+  summary: 'dgbgdsbdgbdgdgdg',
+  content: `dbdbgdsfbdbdfbdgbdgbbdgdgbdg dg g dgbdgbdfbdgd  bdgbdf v dfbdfbdfdbdfbdfbdg`
+};
