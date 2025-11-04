d43269cda28f7a96520366316c51cf681a09ffb1
Abdelbari Saoutelhak <abdelbari.saoutelhak@etu.umontpellier.fr>
2025-11-04 23:55:22 +0100

    Update article: LaTeX Rendering Test

--- data/articles/latex-test.ts
+++ data/articles/latex-test.ts
@@ -15,10 +15,10 @@ This is \\textbf{bold text}, \\textit{italic text}, \\emph{emphasized text}, and
 
 \\subsubsection{Mathematical Expressions}
 
-Inline math: $E = mc^2$ and $\\sum_{i=1}^{n} i = \\frac{n(n+1)}{2}$
+Inline math: \$E = mc^2\$ and \$\\sum_{i=1}^{n} i = \\frac{n(n+1)}{2}\$
 
 Display math:
-$$ \\int_{-\\infty}^{\\infty} e^{-x^2} dx = \\sqrt{\\pi} $$
+\$\$ \\int_{-\\infty}^{\\infty} e^{-x^2} dx = \\sqrt{\\pi} \$\$
 
 Using \\( \\) notation: \\(a^2 + b^2 = c^2\\)
 
@@ -30,25 +30,25 @@ Using \\[ \\] notation:
 \\subsection{Remark Blocks}
 
 \\begin{remarque}
-This is a remark with inline math $x^2 + y^2 = z^2$ and display math:
-$$ \\lim_{n \\to \\infty} \\left(1 + \\frac{1}{n}\\right)^n = e $$
+This is a remark with inline math \$x^2 + y^2 = z^2\$ and display math:
+\$\$ \\lim_{n \\to \\infty} \\left(1 + \\frac{1}{n}\\right)^n = e \$\$
 The remark also supports \\textbf{bold} and \\textit{italic} text.
 \\end{remarque}
 
 \\subsection{Complex Equations}
 
 The Schrödinger equation:
-$$ i\\hbar\\frac{\\partial}{\\partial t}\\Psi(\\vec{r},t) = \\hat{H}\\Psi(\\vec{r},t) $$
+\$\$ i\\hbar\\frac{\\partial}{\\partial t}\\Psi(\\vec{r},t) = \\hat{H}\\Psi(\\vec{r},t) \$\$
 
-Maxwell's equations:
-$$
+Maxwell\'s equations:
+\$\$
 \\begin{aligned}
 \\nabla \\cdot \\vec{E} &= \\frac{\\rho}{\\epsilon_0} \\\\
 \\nabla \\cdot \\vec{B} &= 0 \\\\
 \\nabla \\times \\vec{E} &= -\\frac{\\partial \\vec{B}}{\\partial t} \\\\
 \\nabla \\times \\vec{B} &= \\mu_0\\vec{J} + \\mu_0\\epsilon_0\\frac{\\partial \\vec{E}}{\\partial t}
 \\end{aligned}
-$$
+\$\$
 
 \\section{Conclusion}
 
