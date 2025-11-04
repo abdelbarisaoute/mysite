ecd0ac112898fc8e845bd9ad4d6b05c15a70895c
Abdelbari Saoutelhak <abdelbari.saoutelhak@etu.umontpellier.fr>
2025-11-04 15:41:15 +0100

    Delete article: ervfgbfggnfgfgd

--- data/articles/ervfgbfggnfgfgd.ts
+++
@@ -1,9 +0,0 @@
-import { Article } from '../../types';
-
-export const ervfgbfggnfgfgd: Article = {
-  id: 'ervfgbfggnfgfgd',
-  title: 'ervfgbfggnfgfgd',
-  date: '2025-11-04',
-  summary: 'sgdbgdgsbdfbdfbdgbdgsb',
-  content: `dsgbdfbdtetgetgdf ebegerbdfbdbdb`
-};
